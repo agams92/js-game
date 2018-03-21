1d42ba558ab85cd72a53dd64324b7170f38cba2e
Lev <agams92@gmail.com>
2018-03-21 16:12:47 +0300

    ass Vector is created

--- game.js
+++ game.js
@@ -1,2 +1,17 @@
 'use strict';
-test
+class Vector {
+    constructor(x = 0,y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+    plus(vector) {
+        if (!(vector instanceof Vector)) {
+            throw new Error('Можно прибавлять к вектору только вектор типа Vector');
+        }
+        else {
+            x += vector.x;
+            y += vector.y;
+            return new Vector(x,y);
+        }
+    }
+}
