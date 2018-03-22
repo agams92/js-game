e1910bf713dd8a65cf92146299978c68507f27c3
Lev <agams92@gmail.com>
2018-03-22 09:35:29 +0300

    Class Actor is created

--- game.js
+++ game.js
@@ -9,9 +9,31 @@ class Vector {
             throw new Error('Можно прибавлять к вектору только вектор типа Vector');
         }
         else {
-            x += vector.x;
-            y += vector.y;
-            return new Vector(x,y);
+            this.x += vector.x;
+            this.y += vector.y;
+            return new Vector(this.x,this.y);
         }
     }
+    times(multiplier) {
+        this.x *= multiplier;
+        this.y *= multiplier;
+        return new Vector(this.x,this.y);
+    }
+}
+
+class Actor {
+    constructor(pos = new Vector(0,0),size= new Vector(1,1),speed= new Vector(0,0)) {
+        this.pos = pos;
+        this.size = size;
+        this.speed = speed;
+        if(!(this.pos instanceof Vector) || !(this.size instanceof Vector) || !(this.speed instanceof Vector)) {
+            throw new Error ('Аргумент не является вектором типа Vector')
+        }
+        Object.defineProperty(this,'type',{
+            writable: false,
+            value: 'actor'
+        })
+    }
+    act(){};
+    
 }
